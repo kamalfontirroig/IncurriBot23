b2d4a2dba9fc2c048e18b237eb99c7bcb7561b4a
kfontirr <kfontirr@nttdata.com>
2023-01-10 22:38:49 +0000

    Started working on jobDescription editing capabilities

--- IncurriBot-JiraNttdata2023.py
+++ IncurriBot-JiraNttdata2023.py
@@ -10,18 +10,49 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time 
 
-## User Credentials
+## Visit https://github.com/kamalfontirroig/IncurriBot23/ for the lastest version
+## You need selenium and webdriver_manager packages install for this bot to run
+#######
+#######
+## Add your user Credentials
+## Else, leave it as it is and manually enter your credentials when the site login opens
 userLogin = "usuario"
 userPassword = "contraseña"
 
+## TODO: Load the days we are incurring form CSV into the tuple daysToIncurr
+## TODO: The bot should incurr into multiple Jiras by setting them in the daysToIncurr array 
+
+## The bot will take the date, hours to incurr, and jobDescription from this array of tuples. 
+## Day must start with 0 if it's lower than 
+## Month must be set as the first 3 letters of the Month name, in lower case.
+## Be sure of your daysToIncurr array before running the bot, it will not rollback what has been already incurred before failure.
+## You can set multiple incurring data for the same date, by adding a tuple with the same date
+## For Example:
+## [('12/ene/2023 9:00 AM','6h','Worked Hard'), ('12/ene/2023 3:00 PM','2h','Worked kinda hard'), ('12/ene/2023 6:00 PM','1h','Picked my belly button')]
+daysToIncurr = [('01/feb/2023 9:00 AM','6h','Worked Hard'), ('01/feb/2023 3:00 PM','2h','Worked kinda hard'), ('01/feb/2023 6:00 PM','1h','Picked my belly button')]
+
+##This is the site where the bot will incurr the above daysToIncurr array
+## For example: "https://umane.everis.com/jiraito/browse/HPCCHILE-96"
+jiraToIncurrUrl = "https://umane.everis.com/jiraito/browse/HPCCHILE-96"
+
+## if Two Factor Aunthentication (2FA) is set - as it should be :judgy_eyes: - the bot will only start after it is succesfully login.
+## For this, it uses a loginProof, that can be any element's xpath that will be present in the page we are redirected to after succesful 2FA
+## For example: "//a[@data-issue-key='HPCCHILE-84']"
+loginProof = "//a[@data-issue-key='HPCCHILE-84']"
+##########
+##########
+
+
+##Bot starts here
 options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver = webdriver.Chrome(chrome_options=options)
 
-driver.get("https://umane.everis.com/jiraito/browse/HPCCHILE-96")
+driver.get(jiraToIncurrUrl)
 
 ## Login In
+WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, 'userNameInput')))
 userInput = driver.find_element("xpath","//input[@id='userNameInput']")
 userInput.send_keys(userLogin)
 passwordInput = driver.find_element("xpath","//input[@id='passwordInput']")
@@ -30,24 +61,20 @@ passwordInput.send_keys(userPassword)
 driver.find_element("xpath","//span[@id='submitButton']").click()
 
 ## Loop until we have logged in with 2FA
-## loginProof can be any element from the page we are redirected after succesful 2FA
-loginProof = "//a[@data-issue-key='HPCCHILE-84']"
 twoFANotDone = True
 while twoFANotDone :
     print("Waiting for 2FA completion")
+    time.sleep(1)
     try:
         if type(driver.find_element("xpath",loginProof)) is WebElement :
             twoFANotDone = False
     except NoSuchElementException:
         twoFANotDone = True        
-print("We are in")
-
-## TODO: Load the days we are incurring form CSV into the tuple daysToIncurr
-daysToIncurr = [('12/ene/2023 9:00 AM','9h'),('13/ene/2023 9:00 AM','9h'),('16/ene/2023 9:00 AM','9h'),('17/ene/2023 9:00 AM','9h'),('18/ene/2023 9:00 AM','9h')]
+print("NEO: We are in")
 
 ## Now incurr the loaded days into Jira
 print("Incurring Started")
-for incurringDay, workedHours in daysToIncurr :
+for incurringDay, workedHours, jobDescription in daysToIncurr :
     try:
         WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, 'opsbar-operations_more')))
         driver.find_element("xpath","//a[@id='opsbar-operations_more']").click()
@@ -57,6 +84,22 @@ for incurringDay, workedHours in daysToIncurr :
         driver.find_element("xpath","//input[@id='log-work-time-logged']").send_keys(workedHours)
         driver.find_element("xpath","//input[@id='log-work-date-logged-date-picker']").clear()
         driver.find_element("xpath","//input[@id='log-work-date-logged-date-picker']").send_keys(incurringDay)
+        
+        ### TODO: It should be able to send keys to Job Description
+        ### at this point it fails to interact with the element
+        #WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, 'tinymce')))
+        #textArea = driver.find_element("xpath","//body[@id='tinymce']")
+        #textArea.click()
+        #time.sleep(1)
+        #textArea = driver.find_element("xpath","//body[@id='tinymce']/p")
+        #textArea.click()
+        #innerHtml = f"arguments[0].innerText = '{jobDescription}'"
+        #driver.execute_script(innerHtml, textArea)
+        ##driver.find_element("xpath","//body[@id=''tinymce]/p']").clear()
+        ##driver.find_element("xpath","//body[@id=''tinymce]/p]").send_keys(jobDescription)
+        ### 
+
+        ##Here it submits the logged work (incurring)
         WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.ID, 'log-work-submit')))
         time.sleep(1)
         driver.find_element("xpath","//input[@id='log-work-submit']").click()
@@ -64,7 +107,7 @@ for incurringDay, workedHours in daysToIncurr :
         ## Sleeping is needed for the page to load after submiting work
         time.sleep(5)
     except TimeoutException:
-        print("Did the incurring window open?")
+        print("Did the log work window open?")
 
 ## Script is done
 print("Hey! We are done incurring")   
